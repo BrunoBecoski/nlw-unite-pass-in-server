7af7fa21ffc8a7f927f5e5303090eb98468d49ef
Bruno Becoski <brunobecoski@gmail.com>
2024-04-06 13:14:42 -0300

    Gera o build

---
+++ dist/routes/get-event-attendees.mjs
@@ -0,0 +1,7 @@
+import {
+  getEventAttendees
+} from "../chunk-KT4PI55I.mjs";
+import "../chunk-JV6GRE7Y.mjs";
+export {
+  getEventAttendees
+};
---
+++ dist/server.mjs
@@ -0,0 +1,65 @@
+import {
+  errorHandler
+} from "./chunk-SJUXIQ4B.mjs";
+import {
+  checkIn
+} from "./chunk-EFMAK5UM.mjs";
+import {
+  createEvent
+} from "./chunk-RW5OIPTP.mjs";
+import "./chunk-KDMJHR3Z.mjs";
+import {
+  getAttendeeBadge
+} from "./chunk-J753VSPY.mjs";
+import {
+  getEventAttendees
+} from "./chunk-KT4PI55I.mjs";
+import {
+  getEvent
+} from "./chunk-TFLH455M.mjs";
+import {
+  registerForEvent
+} from "./chunk-KKK5HI6B.mjs";
+import "./chunk-JRO4E4TH.mjs";
+import "./chunk-JV6GRE7Y.mjs";
+
+// src/server.ts
+import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+var app = fastify();
+app.register(fastifyCors, {
+  origin: "*"
+});
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    info: {
+      title: "pass-in-server",
+      description: "Especifica\xE7\xF5es da API para o back-end da aplica\xE7\xE3o pass.in constru\xEDda durante o NLW Unite da Rocketseat.",
+      version: "1.0.0"
+    }
+  },
+  transform: jsonSchemaTransform
+});
+app.register(fastifySwaggerUI, {
+  routePrefix: "/docs"
+});
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+app.register(createEvent);
+app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeeBadge);
+app.register(checkIn);
+app.register(getEventAttendees);
+app.setErrorHandler(errorHandler);
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+  console.log("\u{1F680} HTTP server running!");
+});
+export {
+  app
+};
